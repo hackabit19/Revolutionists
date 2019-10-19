ba0581a08cc2c4904c4f6814590806b44a6bce09
PINAKSH KUMAR <36802422+wireshark10@users.noreply.github.com>
2019-10-19 15:14:30 +0530

    mapping and loader files

---
+++ loader.php
@@ -0,0 +1,18 @@
+use Elasticsearch\Clientbuilder;
+require vendor/autoload.php;
+
+$dotenv = new Dotenv\Dotenv(__DIR__);
+    $dotenv->load();
+$elasticsearch_host = getenv('ELASTICSEARCH_HOST');
+
+
+// connecting to elasticsearch_host
+$hosts = [
+      [
+        'host' => $elasticsearch_host
+      ]
+    ];
+
+    $client = ClientBuilder::create()->setHosts($hosts)->build();
+
+
---
+++ map.php
@@ -0,0 +1,4 @@
+//including loader file which is loader.php
+
+>?php
+require 'loader.php';
