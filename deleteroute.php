28c01b30bdcec58957b9be0bb0255df893b91deb
PINAKSH KUMAR <36802422+wireshark10@users.noreply.github.com>
2019-10-20 02:57:05 +0530

    Add files via upload

---
+++ deleteroute.php
@@ -0,0 +1,19 @@
+<?php 
+    
+    require 'loader.php';
+
+    $data = json_decode(file_get_contents("php://input"), true);
+
+    $params['index'] = 'places';
+    $params['type'] = 'location';
+    $params['body']['query']['match']['user'] = $data['user']; 
+
+    $result = $client->search($params); 
+         $id = $result['hits']['hits'][0]['_id'];
+
+        unset($params['body']);
+      $params['id'] = $id;
+        $result = $client->delete($params);
+
+    
+        echo json_encode($result);
