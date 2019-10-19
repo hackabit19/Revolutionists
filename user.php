d21f0b48059998dc4b98d49b3510fdc35d961fd8
wireshark10 <jhapinakshthink@gmail.com>
2019-10-19 18:21:12 +0530

    user backend

---
+++ user.php
@@ -0,0 +1,51 @@
+<?php
+
+require 'loader.php' ;
+
+
+$data= json_decode( file_get_contents("php://input" ) , true);
+
+$user= $data['user' ];
+
+$para=[
+
+
+'index' => 'places',
+'type'=> 'users',
+
+
+];
+
+$para['body']['query']['match']['username'] = $user;
+
+try{
+
+
+    $search=$client -> search($para);
+
+if($search['hits']['total']==0)
+{
+
+$response=$client -> index(
+
+[
+
+'index'=> 'places' ,
+'type'=> 'users',
+'id'=> $user,
+'body' => [
+    'username' => $user
+]
+]
+
+);
+
+}
+
+}
+
+   echo 'ok';
+} 
+catch(\Exception $e){
+    echo 'err:'.$e->getMessage();
+}
