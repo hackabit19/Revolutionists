a2c56b2a9f3bfd31264eb40800ca255a6d82e34f
PINAKSH KUMAR <36802422+wireshark10@users.noreply.github.com>
2019-10-20 07:17:55 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-import Tapper from './Tapper';
+/**
+ * @format
+ */
 
-export default Tapper;
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent('RideIT', () => App);
